066f592ec96a22e99010d21376d13a6679d66e97
vagrant <vagrant@vagrant.vm>
2017-06-01 08:32:07 +0000

    文字が入力された場合エラーを返すように変更

--- warikan.js
+++ warikan.js
@@ -3,8 +3,14 @@ var total = document.getElementById('kaikei');
 var man = document.getElementById('ninzu');
 var value = document.getElementById('keisan');
 
+
   /*割り勘式（少数点四捨五入）*/
 function answer() {
-  var sum = Math.round( eval(total.value) / eval( man.value ) );
+  if(typeof(man.value) == "number" && typeof(total.value) == "number"){
+    var sum = Math.round( eval(total.value) / eval( man.value ) );
+  }
+  if(isNaN(sum)){
   swal("お一人様" + sum + "円です", "お疲れ様でした‼", "success");
+  }
+  swal("入力された値が不正です");
 };
