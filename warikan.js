9753554c8925f333bfe58f7955d527573a547847
vagrant <vagrant@vagrant.vm>
2017-06-01 07:57:37 +0000

    warikan.jsにif文追加

--- warikan.js
+++ warikan.js
@@ -6,5 +6,8 @@ var value = document.getElementById('keisan');
   /*割り勘式（少数点四捨五入）*/
 function answer() {
   var sum = Math.round( eval(total.value) / eval( man.value ) );
+  if (total.value == 0 && man.value == 0) {
+    sum = 0
+  }
   swal("お一人様" + sum + "円です", "お疲れ様でした‼", "success");
 };
